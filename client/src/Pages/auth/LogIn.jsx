33ead4f5b4bfc105afaa6f431c3e5484f114aea4
Ashraf Ali <122337346+ashrafali2000@users.noreply.github.com>
2025-01-01 12:54:58 +0500

    Update LogIn.jsx

--- client/src/Pages/auth/LogIn.jsx
+++ client/src/Pages/auth/LogIn.jsx
@@ -7,7 +7,7 @@ import { Link, useNavigate } from "react-router-dom"
 import option2 from '../../assets/json/option2.json'
 import Particle from "../../components/Particle"
 import HomeLayout from "../../Layouts/HomeLayout"
-import { forgotPassword, login } from "../../redux/slices/AuthSlice"
+import { forgotPassword, login } from "../../Redux/slices/AuthSlice"
 function LogIn() {
     const dispatch = useDispatch();
     const navigate = useNavigate();
