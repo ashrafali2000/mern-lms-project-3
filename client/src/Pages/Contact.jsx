cdded073cf9b05268aed57dd489eb891ff5644ec
Ashraf Ali <122337346+ashrafali2000@users.noreply.github.com>
2025-01-01 13:02:05 +0500

    Update Contact.jsx

--- client/src/Pages/Contact.jsx
+++ client/src/Pages/Contact.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react'
 import { toast } from 'react-toastify'
 
-import option2 from '../assets/Json/option2.json'
+import option2 from '../assets/json/option2.json'
 import Particle from '../components/Particle'
 import axiosInstance from '../helpers/AxiosInstance';
-import HomeLayout from '../layouts/HomeLayout'
+import HomeLayout from '../Layouts/HomeLayout'
 function Contact() {
 
     const [userInput, setUserInput] = useState({
